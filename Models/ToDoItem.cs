8a9a3d8d76debd7e249506ea3d46650d62db9509
rulle1987 <rulle1987@gmail.com>
2023-08-08 22:49:42 -0400

    Adding DbContext

---
+++ Models/ToDoItem.cs
@@ -0,0 +1,16 @@
+namespace ToDoGrpc.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public string ToDoStatus { get; set; } = "New";
+        
+    }
+
+        
+}
